97d0a6fd6a4dc379be0e5566b9e7c5abc6f56093
Guillaume <beaupuyg@outlook.fr>
2023-04-06 17:10:12 +0200

    Init project

---
+++ scripts/utils/dropsdown.js
@@ -0,0 +1,4 @@
+const selectTags = document.querySelector(".select-tags");
+const selectBtn = document.querySelector(".select-tags-btn");
+const selectTagsBloc = document.querySelector(".select-tags-bloc");
+const selectTagsLists = document.querySelector(".select-tags-lists");
